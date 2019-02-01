d019dfbc0b313c2370fb9dfba2e28d763dae09d3
Jorge G <jogoram428@gmail.com>
2019-02-01 15:37:09 -0700

    created menu for user

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 let mysql = require("mysql");
 let columnify = require("columnify");
+let inquirer = require('inquirer');
 
 let connection = mysql.createConnection({
     host: "localhost",
@@ -11,7 +12,7 @@ let connection = mysql.createConnection({
 
 connection.connect(function(err){ 
     if(err) throw err;
-    console.log(`**** Successfully connected to Bamazon inventory as ID ${connection.threadId} *****`);
+    console.log(`**** Successfully connected to Bamazon inventory as ID ${connection.threadId} *****\n`);
     displayInventory();
 });
 
@@ -19,7 +20,39 @@ function displayInventory(){
     connection.query("SELECT item_id, product_name, price FROM products", function(err, res){
         if(err) throw err;
         console.log(columnify(res));
-        connection.end();
+        userPrompt();
     });
 };
+
+function userPrompt(){
+    inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View products",
+        "Purchase an item",
+        "Exit"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "View products":
+        displayInventory();
+        break;
+
+      case "Purchase an item":
+        purchaseItem();
+        break;
+
+      case "Exit":
+        console.log(`=====================`);
+        console.log(`Thank you come again!`);
+        console.log(`=====================`);
+        connection.end();
+        break;
+      }
+    });
+}
 
