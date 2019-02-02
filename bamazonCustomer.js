b83d3970ca7358e735682f3550afd8523b5c764e
Jorge G <jogoram428@gmail.com>
2019-02-01 19:05:06 -0700

    updates database based on what customer purchased

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,13 +16,6 @@ connection.connect(function(err){
     displayInventory();
 });
 
-function displayInventory(){
-    connection.query("SELECT item_id, product_name, price FROM products", function(err, res){
-        if(err) throw err;
-        console.log(columnify(res));
-        userPrompt();
-    });
-};
 
 function userPrompt(){
     inquirer
@@ -56,3 +49,59 @@ function userPrompt(){
     });
 }
 
+function displayInventory(){
+    connection.query("SELECT item_id, product_name, price FROM products", function(err, res){
+        if(err) throw err;
+        console.log(columnify(res));
+        userPrompt();
+    });
+};
+
+//function that prompts user to purchase an item based on ID and also an amount they wish to purchase
+function purchaseItem(){
+    inquirer
+    .prompt([{
+      name: "item",
+      type: "input",
+      message: "Enter item ID you wish to purchase:"
+    },
+    {
+      name: "quantity",
+      type: "input",
+      message: "Enter amount you wish to purchase:"
+    }  
+    ])
+    .then(function(answer) {
+        console.log(answer.quantity);
+        connection.query("SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?", {item_id: answer.item}, function(err, res){
+            if(err) throw err;
+            if(parseInt(answer.quantity)>res[0].stock_quantity){
+                console.log("insuffecient supply. Only "+res[0].stock_quantity+ " items in stock please reselect.");
+                purchaseItem();
+            }
+            else{
+            updateInventory(res[0].item_id, res[0].stock_quantity-answer.quantity);
+            console.log("===========================================================================================");
+            console.log(`Successfully purchased ${answer.quantity} ${res[0].product_name} at $${res[0].price} a peice!`);
+            console.log("===========================================================================================");
+            };
+        });
+
+    });
+};
+
+//function that updates the quantity in stock based on amount bought
+function updateInventory(itemId, inStock){
+    let query = "UPDATE products SET ? WHERE ?";
+    connection.query(query, [
+        { 
+            stock_quantity: inStock 
+        },
+        {
+            item_id: itemId
+        }
+    ], function(err, res) {
+        userPrompt();
+    });
+ 
+};
