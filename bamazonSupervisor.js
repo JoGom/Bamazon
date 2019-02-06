84fd6d097f29dbc1605ac7a33dcbb13297c60c1b
Jorge G <jogoram428@gmail.com>
2019-02-05 22:55:39 -0700

    added more options to supervisor menu

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -29,25 +29,62 @@ function userPrompt(){
       ]
     })
     .then(function(answer) {
-      switch (answer.action) {
-      case "View Product Sales by Department":
-        displayDepartmentSales();
+        switch (answer.action) {
+        case "View Product Sales by Department":
+            displayDepartmentSales();
         break;
-
-      case "Create New Department":
-        CreateDepartment();
+        case "Create New Department":
+            CreateDepartment();
         break; 
-
-      case "Exit":
-        console.log(`=====================`);
-        console.log(`Thank you come again!`);
-        console.log(`=====================`);
+        case "Exit":
+            console.log(`=====================`);
+            console.log(`Thank you come again!`);
+            console.log(`=====================`);
         connection.end();
-      break;
-      }
+        break;
+        }
     });
 };
 
 function displayDepartmentSales(){
-    
+    var query = "SELECT department_id, department_name, over_head_costs, SUM(product_sales) Product_sales, SUM(product_sales) - over_head_costs Total_profits ";
+    query += "FROM departments INNER JOIN products USING (department_name) ";
+    query += "GROUP BY department_name";
+    connection.query(query, function(err, res){
+        if(err) throw err;
+        console.log("===============================================================================");
+        console.log(columnify(res));
+        console.log("===============================================================================");
+        userPrompt();
+    });
 };
+
+function CreateDepartment(){
+    console.log("hello");
+    inquirer
+    .prompt([{
+        name: "department",
+        type: "input",
+        message: "Enter department name: "
+    },
+    {
+        name: "cost",
+        type: "input",
+        message: "Enter the department's over head cost: "
+    }
+    ])
+    .then(function(answer) {
+        let query = "INSERT INTO departments SET ?";
+        connection.query(query, {
+            department_name: answer.department,
+            over_head_costs: answer.cost
+        }, 
+        function(err, res){
+            if(err) throw err;
+            console.log("===============================================================================");
+            console.log(`Successfully added ${answer.department} as a new department!`);
+            console.log("===============================================================================");
+            userPrompt();
+        });
+    });
+}
